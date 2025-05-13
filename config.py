a3b7fa19b5aa399cfecf56255080b05cafbd46c2
Dinil Hansana <145005610+DinilHansana@users.noreply.github.com>
2025-05-14 04:39:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ PBKDF2_ITERATIONS = 100000
 BACKUP_FOLDER = "backups"
 QR_FOLDER = "2fa_qr"
 
-# Create necessary folders
+
 os.makedirs(BACKUP_FOLDER, exist_ok=True)
 os.makedirs(QR_FOLDER, exist_ok=True)
